b9d75c70eca9c87c1798b7f6e62a4de2a45a6be7
[sama durani] <[duranisama@gmail.com]>
2024-09-17 19:19:40 -0400

    complete lab

--- server/app.py
+++ server/app.py
@@ -20,19 +20,23 @@ def index():
 
 @app.route('/bakeries')
 def bakeries():
-    return ''
+    bakeries = Bakery.query.all()
+    return [bakery.to_dict() for bakery in bakeries], 200
 
 @app.route('/bakeries/<int:id>')
 def bakery_by_id(id):
-    return ''
+    bakery = Bakery.query.filter(Bakery.id == id).first()
+    return bakery.to_dict(), 200
 
 @app.route('/baked_goods/by_price')
 def baked_goods_by_price():
-    return ''
+    baked_goods = BakedGood.query.order_by(BakedGood.price.desc()).all()
+    return [baked_good.to_dict() for baked_good in baked_goods], 200
 
 @app.route('/baked_goods/most_expensive')
 def most_expensive_baked_good():
-    return ''
+    baked_good = BakedGood.query.order_by(BakedGood.price.desc()).first()
+    return baked_good.to_dict(), 200
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
